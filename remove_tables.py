04e498b71e4630e4691c7b4292cbe7f887ef8941
Akash Kumar <74480055+indexedakki@users.noreply.github.com>
2026-01-26 14:40:25 +0530

    improved

--- remove_tables.py
+++ remove_tables.py
@@ -4,7 +4,8 @@ import io
 import fitz
 import boto3, fitz
 import configparser
- 
+import shutil
+
 config = configparser.ConfigParser()
 config.read('configs/config.ini')
  
@@ -17,7 +18,9 @@ DOLLAR_THRESHOLD = 30
 textract = boto3.client(service_name = 'textract', region_name = aws_region, aws_access_key_id = awsAccessKey, aws_secret_access_key = awsSecretKey)
  
 def find_truncation_page(pdf_path):
-    doc = fitz.open(pdf_path)
+    with open(pdf_path, "rb") as f:
+        pdf_bytes = f.read()
+    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
  
     for i, page in enumerate(doc):
         pix = page.get_pixmap(dpi=300)
@@ -40,41 +43,56 @@ def find_truncation_page(pdf_path):
     return None
  
 def truncate_pdf(input_pdf, output_pdf):
-    truncate_from = find_truncation_page(input_pdf)
-    doc = fitz.open(input_pdf)
- 
-    # No truncation → save full PDF
-    if truncate_from is None:
-        print("ℹ️ No truncation needed, len of doc:", len(doc))
-        doc.save(output_pdf)
+    try:
+        truncate_from = find_truncation_page(input_pdf)
+        with open(input_pdf, "rb") as f:
+            pdf_bytes = f.read()
+        doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+    
+        # No truncation → save full PDF
+        if truncate_from is None:
+            print("ℹ️ No truncation needed, len of doc:", len(doc))
+            doc.save(output_pdf)
+            doc.close()
+            return True
+    
+        new_doc = fitz.open()
+        if truncate_from > 0:
+            print("🛑 Truncating from page:", truncate_from, "Total pages:", len(doc))
+            new_doc.insert_pdf(doc, from_page=0, to_page=truncate_from - 1)
+    
+        new_doc.save(output_pdf)
         doc.close()
-        return
- 
-    new_doc = fitz.open()
-    if truncate_from > 0:
-        print("🛑 Truncating from page:", truncate_from)
-        new_doc.insert_pdf(doc, from_page=0, to_page=truncate_from - 1)
- 
-    new_doc.save(output_pdf)
-    doc.close()
-    new_doc.close()
+        new_doc.close()
+        return True
+    except Exception as e:
+        print("❌ Error during truncation:", str(e))
+        return False
  
  
 def process_folder():
+    start_time = os.times()
     for file in os.listdir(INPUT_FOLDER):
         if not file.lower().endswith(".pdf"):
             continue
- 
+        
+        print(f"🔍 Processing file: {file}")
         input_path = os.path.join(INPUT_FOLDER, file)
         output_path = os.path.join(OUTPUT_FOLDER, file)
  
-        print(f"📄 Processing: {file}")
-        truncate_pdf(input_path, output_path)
-        print(f"✅ Saved: {output_path}")
- 
+        res = truncate_pdf(input_path, output_path)
+        if res:
+            shutil.move(input_path, os.path.join(PROCESSED_FOLDER, file))
+
+    end_time = os.times()
+    print(f"⏱️ Total processing time: {end_time.elapsed - start_time.elapsed} seconds")
+
 INPUT_FOLDER = "pdfs"
+PROCESSED_FOLDER = "pdfs/processed_pdfs"
 OUTPUT_FOLDER = "clean_pdfs"
- 
+
+os.makedirs(INPUT_FOLDER, exist_ok=True)
+os.makedirs(PROCESSED_FOLDER, exist_ok=True)
 os.makedirs(OUTPUT_FOLDER, exist_ok=True)
  
 if __name__ == "__main__":
